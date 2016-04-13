83c61e0de606969b59b7fa55fc35ab69cd2cde7a
Mariusz Sikora <msikora87@gmail.com>
2016-04-13 21:01:21 +0200

    Init commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+int main(int argc, char **argv[])
+{
+
+    return 0;
+}
---
+++ src/example.h
@@ -0,0 +1,6 @@
+#ifndef _EXAMPLE_
+#define _EXAMPLE_
+
+double add_numbers(const double f1, const double f2);
+
+#endif //_EXAMPLE_
